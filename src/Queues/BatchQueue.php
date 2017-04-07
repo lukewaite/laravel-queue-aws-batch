857b5dcddc039a7c07616dc93d425ce7fcfc467e
Luke Waite <lwaite@gmail.com>
2017-04-07 02:00:27 +0000

    Apply fixes from StyleCI

--- src/Queues/BatchQueue.php
+++ src/Queues/BatchQueue.php
@@ -67,7 +67,6 @@ class BatchQueue extends DatabaseQueue
         } else {
             return is_string($job) ? explode('@', $job)[0] : null;
         }
-
     }
 
     /**
@@ -81,7 +80,6 @@ class BatchQueue extends DatabaseQueue
      */
     protected function pushToBatch($queue, $payload, $jobName)
     {
-
         $jobId = $this->pushToDatabase(0, $queue, $payload);
 //        $jobId = $this->pushToDatabase( $queue, $payload);
 
