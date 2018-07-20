3a25d14e7cb3fb6c8d769d92ce0b93d08961ed3d
Luke Waite <lwaite@gmail.com>
2018-07-20 12:18:32 -0400

    Fix: Jobs sometimes run until max tries hit without hitting laravel's failure method'
    Now referencing the AWS Batch provided AWS_BATCH_JOB_ATTEMPT environment variable
    to get job try count. Fall back to the db retry count if the environment variable
    doest not exist.

--- src/Jobs/BatchJob.php
+++ src/Jobs/BatchJob.php
@@ -43,4 +43,18 @@ class BatchJob extends DatabaseJob
 
         $this->database->release($this->queue, $this->job, 0);
     }
+
+    /**
+     * We override the base database attempts method to try to read out the
+     * `AWS_BATCH_JOB_ATTEMPT` environment variable. If that fails, we fallback
+     * to the db attempts count.
+     *
+     * https://docs.aws.amazon.com/batch/latest/userguide/job_env_vars.html
+     *
+     * @return int
+     */
+    public function attempts()
+    {
+        return env('AWS_BATCH_JOB_ATTEMPT', parent::attempts());
+    }
 }
